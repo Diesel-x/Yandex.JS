ac8f5ad9a531f4214b13ef3c4e3ca4593d6b6e9d
Bayanov Diyaz <92308937+Diesel-x@users.noreply.github.com>
2024-03-03 17:52:07 +0500

    ывфывф

--- Pindie/pindie/app/page.js
+++ Pindie/pindie/app/page.js
@@ -4,15 +4,14 @@ import "./globals.css";
 
 import { Banner } from "./components/Banner/Banner";
 import { Promo } from "./components/Promo/Promo";
-import { NewCardsList } from "./components/CardsList/NewCardsList";
-import { PopularCardsList } from "./components/CardsList/PopularCardsList";
+import { CardsList } from "./components/CardsList/CardsList";
 
 export default function Home() {
   return (
     <main className="main">
       <Banner />
-      <PopularCardsList />
-      <NewCardsList />
+      <CardsList id="popular" title="Популярное"></CardsList>
+      <CardsList id="new" title="Новинки"></CardsList>
       <Promo />
     </main>
   );
